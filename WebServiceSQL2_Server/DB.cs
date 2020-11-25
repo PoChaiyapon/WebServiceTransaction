8a51667426198daee0df50e395e3135427e405fe
Chaiyapon <chaiporn137@gmail.com>
2020-11-25 11:28:35 +0700

    upload file

---
+++ WebServiceSQL2_Server/DB.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceSQL2_Server
+{
+    public class DB
+    {
+        private static String DBSERVER = string.Empty;
+        private static String DBNAME = string.Empty;
+        private static String DBUSER = string.Empty;
+        private static String DBPASS = string.Empty;
+
+        public static String ConnectionStr { get { return String.Format("Data Source={0}; Initial Catalog={1}; User ID={2}; Password={3}", DBSERVER, DBNAME, DBUSER, DBPASS); } }
+
+        public static void SetConfig(string _dbserv, string _dbname, string _dbuser, string _dbpass)
+        {
+            DBSERVER = _dbserv;
+            DBNAME = _dbname;
+            DBUSER = _dbuser;
+            DBPASS = _dbpass;
+        }
+
+        //Check Sql connection
+        public static bool Check_SqlConnect()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(ConnectionStr);
+                conn.Open();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static DataSet execSQL(string sql)
+        {
+            SqlConnection conn = new SqlConnection(ConnectionStr);
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = conn.CreateCommand();
+            try
+            {
+                cmd.CommandText = sql;
+                da.SelectCommand = cmd;
+                DataSet ds = new DataSet();
+                conn.Open();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+                da.Dispose();
+                cmd.Dispose();
+            }
+        }
+    }
+}
---
+++ WebServiceSQL2_Server/WebService1.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Services;
+
+namespace WebServiceSQL2_Server
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        //set config
+        [WebMethod]
+        public void setConfig(string _dbserv, string _dbname, string _dbuser, string _dbpass)
+        {
+            DB.SetConfig(_dbserv, _dbname, _dbuser, _dbpass);
+        }
+
+        //Check connection
+        [WebMethod]
+        public bool Check_Connection()
+        {
+            return DB.Check_SqlConnect();
+        }
+
+        //Return DataSet
+        [WebMethod]
+        public DataSet execSQL(string sql)
+        {
+            return DB.execSQL(sql);
+        }
+    }
+}
